42f681f8bbe54b48a6fae2be5f5827b869c32d6d
Artur <82472015+benyaar@users.noreply.github.com>
2025-07-06 23:00:22 +0300

    Storing Messages With MongoDB In A Database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,13 +1,15 @@
-function handler(req, res) {
-    if(req.method === 'POST'){
-        const {email, name, message} = req.body
-        if(!email || !email.includes('@') || name.trim() === '' || !name || !message || message.trim === ''){
-            res.status(422).json({message: 'Invalid input'})
+async function handler(req, res) {
+    if (req.method === 'POST') {
+        const { email, name, message } = req.body
+        if (!email || !email.includes('@') || name.trim() === '' || !name || !message || message.trim === '') {
+            res.status(422).json({ message: 'Invalid input' })
         }
         const newMessage = {
             email, name, message
         }
-        res.status(201).json({message: 'Success', newMessage})
+        const result = await db.collection('messages').insertOne(newMessage)
+        res.status(201).json({ message: 'Added message', message: result.insertedId })
+        return
     }
 }
 
